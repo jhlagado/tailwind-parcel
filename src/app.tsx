1219ebb2439745256f871300e41f4aa82a4769ca
John Hardy <john.hardy@workjam.com>
2021-04-28 22:33:06 +1000

    minimal react app with ts and tailwind

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl text-purple font-sans">Greetings.</h1>
+    </div>
+  );
+};
+App.displayName = 'App';
